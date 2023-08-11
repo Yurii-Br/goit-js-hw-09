891b51566a0425e880ac055f9a3e06ecb219d48b
Yurii-Br <127565054+Yurii-Br@users.noreply.github.com>
2023-08-11 09:11:11 +0000

    Deploying to gh-pages from @ Yurii-Br/goit-js-hw-09@43e19cfb30e404396c06510ddecc09d034bf0dce 🚀

--- 01-color-switcher.6d15aafa.js
+++
@@ -1,2 +0,0 @@
-const t=document.body;let a,e=!1;function r(){let a=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=a}t.addEventListener("click",(t=>{const o=t.target;o.hasAttribute("data-start")&&!1===e&&(a=setInterval(r,1e3),e=!0),o.hasAttribute("data-stop")&&(clearInterval(a),e=!1)}));
-//# sourceMappingURL=01-color-switcher.6d15aafa.js.map
